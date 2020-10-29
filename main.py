cb7b29e66008aff48996895d6bc22b5fc7dc247a
IT-Kuantum <IT-Kuantum@yandex.ru>
2020-10-29 16:41:34 +1000

    0

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-'''
-#Классы
-class Play():
-#Пренадлежность изображения в классе
-self
-'''
+import pygame as pg
 
+SIZE = WIDTH, HEIGHT = 800, 600
+GRAY = (128, 128, 128)
+
+pg.init()
+pg.display.set_caption('Rally')
+screen = pg.display.set_mode(SIZE)
+
+game = True
+while game:
+    for e in pg.event.get():
+        if e.type == pg.QUIT:
+            game = False
+
+    screen.fill(GRAY)
+    pg.display.update()
 
