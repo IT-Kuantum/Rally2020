81584be80b6dc8de34477544b2030b8faa815a90
IT-Kuantum <IT-Kuantum@yandex.ru>
2020-11-05 16:32:10 +1000

    0

--- main.py
+++ main.py
@@ -69,13 +69,8 @@ while game:
         if e.type == pg.QUIT:
             game = False
 
-    screen.fill(GRAY)
-    #bg()
-    for i in range (2):
-        screen.blit(bg_image, bg_image_rect if i == 0 else bg_image_2_rect)
-    #screen.blit(bg_image, bg_image_2_rect)
-    screen.blit(car1_image, car1.rect)
-    screen.blit(play.image, (play.x, play.y))
+    all.sprite.update()
+    all.sprite.draw(screen)
     pg.display.update()
     clock.tick(FPS)
     pg.display.set_caption(f'Rally   FPS: {int(clock.get_fps())}')
