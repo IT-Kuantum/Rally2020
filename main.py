3cc5cd06b16431f1cb67db4a978ec4f6941ea8ba
IT-Kuantum <IT-Kuantum@yandex.ru>
2020-11-05 16:49:56 +1000

    0

--- main.py
+++ main.py
@@ -27,7 +27,11 @@ class Player(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.image.load('Image/car4.png')
+        self.x, self.y = WIDTH // 2, HEIGHT // 2
         self.rect = self.image.get_rect()
+        self.position = pg.math.Vector2(self.x, self.y)
+        self.velocity = pg.math.Vector2()
+
 
 class Car(pg.sprite.Sprite):
     def __init__(self, x, y, img):
